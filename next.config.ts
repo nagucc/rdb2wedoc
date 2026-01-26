a6220f28fbbfea8bc71238af1bf28153aec8add7
na57 <na57@itc.ynu.edu.cn>
2026-01-26 18:29:08 +0800

    feat: 添加实验性 instrumentationHook 配置

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
   output: 'standalone',
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
